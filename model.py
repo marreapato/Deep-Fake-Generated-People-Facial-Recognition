f80a202570d4c3c37ee286ccf1d51af6f847cbd3
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-02-17 15:52:57 -0300

    Update model.py

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ import cv2
 model = keras.models.load_model('model_dfake-face_softmax.h5')
 
 def image_pre(image_data):
-    img = img = cv2.imdecode(np.frombuffer(image_data, np.float32), -1)
+    img = cv2.imdecode(np.frombuffer(image_data, np.float32), -1)
     img = cv2.resize(img,(128,128))
     img = np.array(img)
     data = img.reshape((-1,128,128,3))
